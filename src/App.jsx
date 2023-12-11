5bf611074c572ee14e5cb2bfab9327a74ed4c20c
Ranahansa <CODSE213F-053@student.nibm.lk>
2023-12-11 16:51:02 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import './App.css';
 function App() {
 
   return (
-    <>
-      
-    </>
+      <div className="App bg-red-100 w-screen h-screen">
+        <div className='container mx-auto'>
+        <h1 className="text-5xl font-bold underline text-red-600 h-40 flex items-center justify-center">Birthday Reminder</h1>
+      </div>
+    </div>
   )
 }
 
