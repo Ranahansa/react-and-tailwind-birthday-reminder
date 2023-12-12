27882c9ecfae387de1e29470c2ed838892791964
Ranahansa <CODSE213F-053@student.nibm.lk>
2023-12-12 09:45:51 +0530

    commit 3

---
+++ src/components/Data.jsx
@@ -0,0 +1,26 @@
+export default [
+    {
+        id : 1,
+        name : "Stepani",
+        age : 25,
+        image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ocW9x-t2gGJPKg3iqvoEfm7qJSB9ujqnqA&usqp=CAU",
+    },
+    {
+        id : 2,
+        name : "Jane",
+        age : 24,
+        image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgWuXcohaDlgXyzUoymgiTQaVZRZJMiczWeqtvCP6mqMJAy1Vsl4QYrrhhtWKkBodYZlE&usqp=CAU",
+    },
+    {
+        id : 3,
+        name : "Jack",
+        age : 23,
+        image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCRAouUpqI61-RFexMr9J09Nqn2W3WLNsejQ&usqp=CAU",
+    },
+    {
+        id : 4,
+        name : "Jill",
+        age : 22,
+        image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ68xCJyjzwUC0J89fXPOkmIvW09vTZjHRkVg&usqp=CAU",
+    }
+];
